ecb91607d6e7a2385739ec29bf3e5f85ab14144f
Ragil}{P <44290386+InYourG00D1@users.noreply.github.com>
2021-08-23 00:44:38 +0700

    Update Go-Pay.php

--- Go-Pay.php
+++ Go-Pay.php
@@ -1,8 +1,6 @@
 <?php
 /*
-Welcome To Tools Prank :V 
-Jangan Dibawa Serius serius amat nanti sakit 
-Bwahaha
+Hayoo mau ngapain
 */
 
 echo "\n\e[0;31m]
@@ -14,17 +12,16 @@ echo "\n\e[0;31m]
     // /   /  /*    *--
    //          //..\\
 +=============UU====UU=======================+
-+              *//||\\*                       +
-+               ****                         +
-+ coded by : InYourG00D                      +
-+ Wa : 6281382406878                         +
-+ Team : LNX#CREW S.T.C BUFT                 +
-+ GitHub : https://github.com/InYourG00D1    +
++              *//||\\*                      +
++                ****                        +
++ coded by : InYourG00D | Ragil              +
++ Team     : LNX#CREW   | S.T.C | BUFT         +
++ GitHub   : https://github.com/InYourG00D1  +
 +============================================+
                               Version 1.1.1\e[0m \r\n";
 echo "\n\e[0;36m]
 --==::{Nuyul Go-Pay}::==--";                              
-echo "\n\e[0;34m]Note 62xxx jangan 08xxx";
+echo "\n\e[0;34m]Example : 08××××××××";
 echo "\n\e[0;33m]
 Nomer: \e[0m \r\n";
 $no = trim(fgets(STDIN, 1024));
@@ -32,7 +29,7 @@ echo "\n\e[0;33m]
 Jumblah Go-Pay: \e[0m \r\n";
 $ju = trim(fgets(STDIN, 1024));
 echo "\n\e[0;32m]
-$no , Sukses Ngirim Saldo Go-Pay , $ju\e[0m \r\n";
+$no , Sukses Ngirim Saldo Gopay , $ju\e[0m \r\n";
 echo "\n\e[0;34m]
 Wait 2jam Gan\e[0m \r\n";
 echo "\n\e[0;34m]
